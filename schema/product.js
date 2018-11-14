2e3b774a9cf4a6279a2774d4d39f8d791e0718b8
Oleksandr <targonsky97@gmail.com>
2018-11-14 13:22:34 +0200

    feat: create schema for product

---
+++ schema/product.js
@@ -0,0 +1,21 @@
+export default `
+  type Product {
+    id: Int!
+    owner: Profile!
+    type: String!
+    title: String!
+    desc: String!
+    price: Float!
+    deliveryType: String!
+    inStock: Boolean!
+  }
+
+  type createProduct {
+    ok: Boolean!
+    errors: [Error!]
+  }
+
+  type Mutation {
+    createProduct(type: String!, title: String!, desc: String!, price: Float!, deliveryType: String!): createProduct!
+  }
+`;
