6d971ed5d773ce967b5e1baa2fa3a6084a0ae1e3
Oleksandr <targonsky97@gmail.com>
2018-12-11 10:37:31 +0200

    feat: add delete section schema

--- api/graphql/schema/section.js
+++ api/graphql/schema/section.js
@@ -27,9 +27,15 @@ export default `
     errors: [Error!]
   }
 
+  type deleteSection {
+    ok: Boolean!
+    errors: [Error!]
+  }
+
   type Mutation {
     createSection(name: String!, type: String!, style: String!, content: String): crateSection!
     updateSectionStyle(sectionId: Int!, style: String!): updateSectionStyle!
     updateSectionContent(sectionId: Int!, type: String, name: String, content: String): updateSectionContent!
+    deleteSection(sectionId: Int!): deleteSection!
   }
 `;
