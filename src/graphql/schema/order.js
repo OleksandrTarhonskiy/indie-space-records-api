6dfe1b21a33a0c729dfef911b9c94753c336900a
Oleksandr <targonsky97@gmail.com>
2019-04-03 11:55:54 +0300

    feat: add schema

---
+++ src/graphql/schema/order.js
@@ -0,0 +1,21 @@
+export default `
+  type Order {
+    id: Int!
+    product: Product!
+    quantity: Int!
+    firstName: String!
+    lastName: String!
+    phoneNumber: String!
+    email: String!
+    deliveryType: String!
+  }
+
+  type createOrder {
+    ok: Boolean!
+    errors: [Error!]
+  }
+
+  type Mutation {
+    createOrder(products: String!, firstName: String!, lastName: String!, phoneNumber: String!, email: String!, deliveryType: String!): createOrder!
+  }
+`;
